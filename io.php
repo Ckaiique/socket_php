cb1ec7997c6df64fb2a4a4c7be5a7b9a645f4952
kaique correia chagas <80991704+Ckaiique@users.noreply.github.com>
2022-04-21 15:53:39 -0300

    Initial commit

---
+++ io.php
@@ -0,0 +1,36 @@
+<?php
+
+$streamList = [
+    stream_socket_client('tcp://localhost:8080'),
+    stream_socket_client('tcp://localhost:8001'),
+    fopen('arquivo.txt','r'),
+    fopen('arquivo2.txt','r'),
+];
+fwrite($streamList[0],'GET /http-server.php HTTP/1.1' . PHP_EOL . PHP_EOL);
+
+foreach($streamList as $stream){
+    stream_set_blocking($stream,false);
+}
+do {
+    $copyReadStream = $streamList;
+    $numeroDeStreams = stream_select($copyReadStream,$write,$except,0,200000);
+
+    if($numeroDeStreams === 0){
+        continue;
+    }
+
+    foreach ($copyReadStream as $key => $stream){
+        $conteudo = stream_get_contents($stream);
+        $posicaoFimHttp = strpos($conteudo, "\r\n\r\n");
+        if($posicaoFimHttp !== false){
+            echo substr($conteudo, $posicaoFimHttp + 4);
+        }else{
+            echo $conteudo;
+        }
+        unset($streamList[$key]);
+    }
+
+
+} while (!empty($streamList));
+
+echo "Li todos" . PHP_EOL;
---
+++ socket-server.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$socket = stream_socket_server('tcp://localhost:8001');
+
+$con    = stream_socket_accept($socket);
+
+$espera = rand(1,5);
+
+sleep($espera);
+
+fwrite($con,"resposta do socket após $espera segundos");
+fclose($con);
